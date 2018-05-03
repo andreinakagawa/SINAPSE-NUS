d2f3185ee4ee6bea6ea3094a3b91edf0997155b8
Andrei Nakagawa <andreinakagawa@outlook.com>
2018-05-03 11:14:15 +0800

    real time colormap example

---
+++ neuromorphic_tactile_array/heatmap.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import time
+
+# create the figure
+fig = plt.figure()
+ax = fig.add_subplot(111)
+im = ax.imshow(np.random.random((50,50)))
+plt.show(block=False)
+
+# draw some data in loop
+for i in range(100):
+    # wait for a second
+    #time.sleep(1)
+    # replace the image contents
+    im.set_array(np.random.random((50,50)))
+    # redraw the figure
+    #fig.canvas.draw()
+    plt.pause(0.00001)
+    print i
+
+raw_input()
